8b9084d72535744d11941c523520bc0b639b3369
Philip Nettles <philnett@outlook.com>
2019-12-14 05:08:45 -0700

    Add project files.

---
+++ DesktopWatermark/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace DesktopWatermark
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblText = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnPanelClose = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.notify1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.changeTextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Font = new System.Drawing.Font("Segoe Media Center Semibold", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText.ForeColor = System.Drawing.Color.White;
+            this.lblText.Location = new System.Drawing.Point(4, 81);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(71, 25);
+            this.lblText.TabIndex = 1;
+            this.lblText.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 195;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "PNG Files Only";
+            this.openFileDialog1.Filter = "PNG |*png";
+            this.openFileDialog1.Title = "Select a Image";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.btnPanelClose);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.ForeColor = System.Drawing.Color.Blue;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(365, 108);
+            this.panel1.TabIndex = 2;
+            this.panel1.Visible = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.ForeColor = System.Drawing.Color.Black;
+            this.btnSave.Location = new System.Drawing.Point(277, 75);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 12;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(3, 76);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(271, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // btnPanelClose
+            // 
+            this.btnPanelClose.Location = new System.Drawing.Point(344, 2);
+            this.btnPanelClose.Name = "btnPanelClose";
+            this.btnPanelClose.Size = new System.Drawing.Size(18, 23);
+            this.btnPanelClose.TabIndex = 10;
+            this.btnPanelClose.Text = "X";
+            this.btnPanelClose.UseVisualStyleBackColor = true;
+            this.btnPanelClose.Click += new System.EventHandler(this.btnPanelClose_Click);
+            this.btnPanelClose.MouseEnter += new System.EventHandler(this.button2_MouseEnter);
+            this.btnPanelClose.MouseLeave += new System.EventHandler(this.button2_MouseLeave);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.Font = new System.Drawing.Font("Segoe Print", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(206, 50);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(153, 25);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Toggle Scroll On/Off";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Lavender;
+            this.label2.Location = new System.Drawing.Point(5, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 26);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Change Scrolling Text";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lavender;
+            this.label1.Location = new System.Drawing.Point(5, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 26);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Change Image";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(294, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(34, 24);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "..";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(2, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(292, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // notify1
+            // 
+            this.notify1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notify1.Icon = ((System.Drawing.Icon)(resources.GetObject("notify1.Icon")));
+            this.notify1.Text = "WaterDesk";
+            this.notify1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notify1_MouseClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.changeTextToolStripMenuItem,
+            this.selectImageToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(142, 98);
+            // 
+            // changeTextToolStripMenuItem
+            // 
+            this.changeTextToolStripMenuItem.Name = "changeTextToolStripMenuItem";
+            this.changeTextToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.changeTextToolStripMenuItem.Text = "Change Text";
+            this.changeTextToolStripMenuItem.Click += new System.EventHandler(this.changeTextToolStripMenuItem_Click);
+            // 
+            // selectImageToolStripMenuItem
+            // 
+            this.selectImageToolStripMenuItem.Name = "selectImageToolStripMenuItem";
+            this.selectImageToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.selectImageToolStripMenuItem.Text = "Select Image";
+            this.selectImageToolStripMenuItem.Click += new System.EventHandler(this.selectImageToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(39, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(289, 74);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(365, 108);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Opacity = 0.65D;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NotifyIcon notify1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem selectImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem changeTextToolStripMenuItem;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.Button btnPanelClose;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+
+
+
+    }
+}
+
---
+++ DesktopWatermark/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using DesktopWatermark.Properties;
+
+namespace DesktopWatermark
+{
+    public partial class Form1 : Form
+    {
+        private PictureBox box = new PictureBox();
+        AboutBox1 abt = new AboutBox1();
+        public Form1()
+        {
+            InitializeComponent();
+            this.checkBox1.CheckedChanged += new EventHandler(checkBox1_CheckedChanged);
+         //   this.Paint += new PaintEventHandler(DrawString);
+        }
+
+        private void pictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (this.FormBorderStyle == System.Windows.Forms.FormBorderStyle.Fixed3D)
+            {
+                  this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            }
+            else if (this.FormBorderStyle == System.Windows.Forms.FormBorderStyle.None)
+            {
+                this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notify1.Visible = true;
+           
+            checkBox1.Checked = Settings.Default.chkBox;
+            lblText.Text = Settings.Default.Tstring + "                                                   ";
+           // pictureBox1.Image = Image.FromFile(Settings.Default.Img);
+            this.Location = new Point(Screen.PrimaryScreen.Bounds.Width - this.Width, Screen.PrimaryScreen.Bounds.Height - this.Height -30);
+            textBox1.Text = Settings.Default.Img;
+            textBox2.Text = Settings.Default.Tstring;
+            IfBlank();
+        }
+
+        private void IfBlank()
+        {
+            if (Settings.Default.Img == "")
+            {
+                pictureBox1.Image = Properties.Resources.pnett;
+            }
+            else
+            {
+                pictureBox1.Image = Image.FromFile(Settings.Default.Img);
+            }
+        }
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            if (this.FormBorderStyle == System.Windows.Forms.FormBorderStyle.Fixed3D)
+            {
+                this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            }
+            else if (this.FormBorderStyle == System.Windows.Forms.FormBorderStyle.None)
+            {
+                this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            }
+        }
+               
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+             lblText.Text = lblText.Text.Substring(1, lblText.Text.Length - 1) + lblText.Text.Substring(0, 1);
+             Invalidate();
+        }
+
+
+
+        //remove image flicker
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle = cp.ExStyle | 0x2000000;
+                return cp;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            notify1.Visible = false;
+        }
+
+        private void notify1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                contextMenuStrip1.Show(this, this.PointToClient(Cursor.Position));
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                lblText.Text = Settings.Default.Tstring;
+                timer1.Start();
+                this.Height = 108;
+                this.Location = new Point(Screen.PrimaryScreen.Bounds.Width - this.Width, Screen.PrimaryScreen.Bounds.Height - this.Height - 30);
+                lblText.Text = Settings.Default.Tstring + "                                                   ";
+                panel1.Visible = false;              
+            }
+            else if (checkBox1.Checked == false)
+            {
+                timer1.Stop();
+                lblText.Text = "";
+                this.Height = 78;
+                this.Location = new Point(Screen.PrimaryScreen.Bounds.Width - this.Width, Screen.PrimaryScreen.Bounds.Height - this.Height -30);
+                panel1.Visible = false;
+            }
+        }
+
+        private void selectImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Height = 108;
+            panel1.Visible = true;
+            this.Location = new Point(Screen.PrimaryScreen.Bounds.Width - this.Width, Screen.PrimaryScreen.Bounds.Height - this.Height - 30);
+            this.Invalidate();
+            
+        }
+
+        private void changeTextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Height = 108;
+            panel1.Visible = true;
+            this.Location = new Point(Screen.PrimaryScreen.Bounds.Width - this.Width, Screen.PrimaryScreen.Bounds.Height - this.Height -30);
+            this.Invalidate();
+            
+        }
+
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+            btnPanelClose.ForeColor = Color.Red;
+        }
+
+        private void button2_MouseLeave(object sender, EventArgs e)
+        {
+            btnPanelClose.ForeColor = Color.Blue;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result== System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName.ToString());
+
+            }
+
+        }
+
+        private void btnPanelClose_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            lblText.Text = " ";
+            Settings.Default.Tstring = textBox2.Text;
+            Settings.Default.Img = textBox1.Text;
+            Settings.Default.Save();
+            panel1.Visible = false;
+            lblText.Invalidate();
+            lblText.Text = Settings.Default.Tstring + "                                                   ";
+            timer1.Start();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            abt.ShowDialog();
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
